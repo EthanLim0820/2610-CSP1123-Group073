612dffe5602e7aacffe152d47d43a67696f0fa5e
Ethan Lim Hao Weng <esy.lim0820@gmail.com>
2026-04-26 17:03:47 +0800

    added a bad/good/excellent rating and a successful cooked display

--- cookingfunction.js
+++ cookingfunction.js
@@ -9,17 +9,40 @@ function startCooking(index) {
 
   const valueText = document.createElement("p");
   valueText.innerText = "Value:" + slider.value;
+  const resultText = document.createElement("p");
+  resultText.innerText = "try to hit 40 - 60 to get Excellent";
+
   slider.oninput = () =>{
-  valueText.innerText ="Value:" + slider.value;
+    valueText.innerText ="Value:" + slider.value;
+    if (slider.value >= 40 && slider.value <= 60) {
+      resultText.innerText = "Excellent ";
+    }   else if (slider.value >= 30 && slider.valu <= 70) {
+      resultText.innerText = "Great ";
+    }   else{
+      resultText.innerText = "Bad ";
+    }
   };
 
   const btn = document.createElement("button");
   btn.innerText = "Cook";
 
-  btn.onclick = () => finishCooking(index, slider.value);
+  btn.onclick = () => {
+    let foodQuanlity;
+    if (slider.value >= 40 && slider.value <= 60) {
+      foodQuality = "Excellent Food ";
+    } else if (slider.value >= 30 && slider.valu <= 70) {
+      foodQuality = "Good Food ";
+    } else {
+      foodQuality = "Bad Food ";
+    }
+    resultText.innerText ="Cook Successful! You made: " + foodQuality;
+    finishCooking(index, slider.value);
+  };
 
-  box.append(valueText, slider, btn);
+  box.append(valueText, resultText, slider, btn);
   document.body.appendChild(box);
 
-  btn.addEventListener("click", () => box.remove());
+  btn.addEventListener("click", () => {
+    setTimeout(() => box.remove(), 6000);
+  });
 }
