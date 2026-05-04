74b2d45c408fa993de828c20db24c2827045dab8
Ethan Lim Hao Weng <esy.lim0820@gmail.com>
2026-05-04 22:13:38 +0800

    add food, size and wetness option button. Also added some color to it

--- cookingfunction.js
+++ cookingfunction.js
@@ -2,6 +2,36 @@ function startCooking(index) {
   const box = document.createElement("div");
   box.className = "cookUI";
 
+  const foodselect = document.createElement("select");
+  const foods = ["Carrot", "Seeds", "Lettuce", "Fish", "Meat", "Grains"];
+
+  foods.forEach(food => {
+    const option  = document.createElement("option");
+    option.value = food;
+    option.innerText = food;
+    foodselect.appendChild(option);
+  });
+
+  const wetnessSelect = document.createElement("select");
+  const wetnessOptions = ["Dry", "Wet"];
+
+  wetnessOptions.forEach(w => {
+    const option = document.createElement("option");
+    option.value = w;
+    option.innerText = w;
+    wetnessSelect.appendChild(option);
+  });
+
+  const sizeSelect = document.createElement("select");
+  const sizeOptions = ["Small", "Big"];
+
+  sizeOptions.forEach(s => {
+    const option = document.createElement("option");
+    option.value = s;
+    option.innerText = s;
+    sizeSelect.appendChild(option);
+  });
+
   const slider = document.createElement("input");
   slider.type = "range";
   slider.min = 0;
@@ -10,16 +40,20 @@ function startCooking(index) {
   const valueText = document.createElement("p");
   valueText.innerText = "Value:" + slider.value;
   const resultText = document.createElement("p");
-  resultText.innerText = "try to hit 40 - 60 to get Excellent";
+  resultText.innerText = "Try to hit 40 - 60 to get Excellent";
 
   slider.oninput = () => {
-    valueText.innerText ="Value:" + slider.value;
-    if (slider.value >= 40 && slider.value <= 60) {
+    const value = Number(slider.value);
+    valueText.innerText ="Value:" + value;
+    if (value >= 40 && value <= 60) {
       resultText.innerText = "Excellent ";
-    }   else if (slider.value >= 30 && slider.value <= 70) {
+      resultText.style.color = "#4CAF50";
+    }   else if (value >= 30 && value <= 70) {
       resultText.innerText = "Great ";
+      resultText.style.color = "#FFA500";
     }   else{
       resultText.innerText = "Bad ";
+      resultText.style.color = "#E53935";
     }
   };
 
@@ -27,20 +61,31 @@ function startCooking(index) {
   btn.innerText = "Cook";
 
   btn.onclick = () => {
+    btn.disabled = true;
+    slider.disabled = true;
+
+    const value = Number(slider.value);
+    const selectedFood = foodselect.value;
+    const selectWetness = wetnessSelect.value;
+    const selectedSize = sizeSelect.value;
+
     let foodQuality;
 
-    if (slider.value >= 40 && slider.value <= 60) {
-      foodQuality = "Excellent Food ";
-    } else if (slider.value >= 30 && slider.value <= 70) {
-      foodQuality = "Good Food ";
+    if (value >= 40 && value <= 60) {
+      foodQuality = "Excellent Food";
+      resultText.style.color = "#4CAF50";
+    } else if (value >= 30 && value <= 70) {
+      foodQuality = "Good Food";
+      resultText.style.color = "#FFA500";
     } else {
-      foodQuality = "Bad Food ";
+      foodQuality = "Bad Food";
+      resultText.style.color = "#E53935";
     }
-    resultText.innerText ="Cook Successful! You made: " + foodQuality;
+    resultText.innerText = "Cook Successful! You made: " + selectedFood + " ("+ foodQuality + ") [" + selectWetness + ", " + selectedSize + "]";
 
     setTimeout(() => box.remove(), 2000);
   };
 
-  box.append(valueText, resultText, slider, btn);
+  box.append(foodselect, wetnessSelect, sizeSelect, valueText, resultText, slider, btn);
   document.querySelector(".game-box").appendChild(box);
 }
